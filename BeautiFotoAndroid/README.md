7768a48f4d6759ed183513743c9f5bc4fb379095
khacpham <khacpv@gmail.com>
2015-11-14 00:01:34 +0700

    Material Design with Navigation Bar

---
+++ BeautiFotoAndroid/README.md
@@ -0,0 +1,8 @@
+setting up material design theme:
+http://www.androidhive.info/2015/04/android-getting-started-with-material-design/
+
+color palette
+http://www.google.com/design/spec/style/color.html#color-color-palette
+
+
+
---
+++ BeautiFotoAndroid/app/src/main/java/com/oicmap/beautifotoandroid/screen/friend/FriendFragment.java
@@ -0,0 +1,42 @@
+package com.oicmap.beautifotoandroid.screen.friend;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.oicmap.beautifotoandroid.R;
+
+public class FriendFragment extends Fragment {
+
+    public FriendFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_friend, container, false);
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
